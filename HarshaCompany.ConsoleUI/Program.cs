fe54efeaf937e2313b516b13f697fe7175f70d47
Maxim Momotenko <yamahaped@gmail.com>
2023-03-04 22:00:34 +0100

    add

---
+++ HarshaCompany.ConsoleUI/Program.cs
@@ -0,0 +1,106 @@
+﻿using HarshaCompany.Domain.Employees;
+
+Employee.OrganizationName = "Harsha Inc.";
+
+var employees = new List<Employee>();
+
+Console.WriteLine($"Welcome to {Employee.OrganizationName}");
+
+for (var i = 0; i < 10; i++)
+{
+    AddNewEmployee(employees);
+    Console.WriteLine(employees[i].GetEmployeeInformation());
+
+    if (i == 9)
+    {
+        Console.WriteLine($"\nIt was the last empty place in {Employee.OrganizationName}.");
+        break;
+    }
+
+    Console.WriteLine("\nTo add more write 'y' to continue or any other key to stop.");
+
+    if (Console.ReadLine() != "y")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("See you again!");
+
+static void AddNewEmployee(List<Employee> employeesList)
+{
+    Console.WriteLine("\nTo add a new employee You should write some information first.\nWrite id of the new employee.");
+
+    var newEmployee = new Employee("Finance Department");
+
+    while (true)
+    {
+        var input = Console.ReadLine();
+
+        if (int.TryParse(input, out _))
+        {
+            newEmployee.EmpId = int.Parse(input);
+            break;
+        }
+
+        Console.WriteLine("Id shod be integer. Try again.");
+    }
+
+    Console.WriteLine("Write the name.");
+
+    while (true)
+    {
+        var input = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            newEmployee.EmpName = input;
+            break;
+        }
+
+        Console.WriteLine("Name can not be empty! Try again.");
+    }
+
+    Console.WriteLine($"Write {newEmployee.EmpName}`s salary per hour.");
+
+    while (true)
+    {
+        var input = Console.ReadLine();
+
+        if (double.TryParse(input, out _))
+        {
+            var salaryPerHour = double.Parse(input);
+
+            if (salaryPerHour > 0)
+            {
+                newEmployee.SalaryPerHour = salaryPerHour;
+                break;
+            }
+        }
+
+        Console.WriteLine("Salary per hour should be a number and more that 0. Try again.");
+    }
+
+    Console.WriteLine($"Write {newEmployee.EmpName}`s worked hours.");
+
+    while (true)
+    {
+        var input = Console.ReadLine();
+
+        if (double.TryParse(input, out _))
+        {
+            var noOfWorkingHours = double.Parse(input);
+
+            if (noOfWorkingHours > 0)
+            {
+                newEmployee.NoOfWorkingHours = noOfWorkingHours;
+                break;
+            }
+        }
+
+        Console.WriteLine("Worked hours should be a number and more that 0. Try again.");
+    }
+
+    employeesList.Add(newEmployee);
+    Console.WriteLine($"Thanks! {newEmployee.EmpName} has been added to {Employee.OrganizationName}");
+}
---
+++ HarshaCompany.Domain/Employees/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace HarshaCompany.Domain.Employees;
+
+public class Employee
+{
+    public int EmpId;
+    public string? EmpName;
+    public double SalaryPerHour;
+    public double NoOfWorkingHours;
+    public double NetSalary;
+    public static string? OrganizationName;
+    public const string TypeOfEmployee = "Contract Based";
+    public readonly string DepartmentName;
+
+    public Employee(string departmentName)
+    {
+        DepartmentName = departmentName;
+    }
+
+    public string GetEmployeeInformation()
+    {
+        NetSalary = SalaryPerHour * NoOfWorkingHours;
+        return $"\nEmployee id : {EmpId};\nName : {EmpName};\nSalary per hour : {SalaryPerHour};\nNet of work hours : {NoOfWorkingHours};\nNet salary : {NetSalary};\nOrganization name : {OrganizationName};\nType of employee : {TypeOfEmployee};\nDepartment name : {DepartmentName}";
+    }
+}
